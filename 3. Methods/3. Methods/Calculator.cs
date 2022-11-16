025d82fccb4bac6121178125efb04d06c24d2a9b
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-16 19:55:32 +0100

    methods terminado

---
+++ 3. Methods/3. Methods/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Methods
+{
+    public class Calculator
+    {
+
+        public int Add(int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+                sum += number;
+            return sum;
+        }
+    }
+}
---
+++ 3. Methods/3. Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Methods
+{
+
+    public class Program
+	{
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1,2));
+
+        }
+        
+        static void usePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(new Point(40, 60));
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occurred");
+            }
+        }
+    }
+}
