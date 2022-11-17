46577148da70945f7e29ebbaa1e77fd09194768e
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-17 11:30:25 +0100

    indexsers complerado

---
+++ 6. properties/6. properties/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Properties
+{
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var person = new Person(new DateTime(1997, 1, 1));
+			Console.WriteLine(person.Age);
+		}
+	}
+}	
